53f6a32ac78feabea28a42e4ef1e7913f1e49edb
pedrobm1 <pedrobm1rblx@gmail.com>
2024-09-01 23:07:53 -0300

    update studentscontroller

--- University/Models/Student.cs
+++ University/Models/Student.cs
@@ -6,6 +6,7 @@ public class Student
     [Key]
     public int StudentId { get; set; }
     public string Name { get; set; }
+
     public int CourseId { get; set; }
     public Course? Course { get; set; }
 
